42f2ba0153120979e5c1edc03b64900971f6ef50
kalinindenys <kalinindenv@gmail.com>
2016-11-07 17:03:31 +0200

    -registration_handle: add autowired RegistrationService and remove validation method

--- client/src/main/java/com/javaclasses/chatroom/controllers/RegistrationController.java
+++ client/src/main/java/com/javaclasses/chatroom/controllers/RegistrationController.java
@@ -1,26 +1,28 @@
 package com.javaclasses.chatroom.controllers;
 
+import com.javaclasses.chatroom.RegistrationService;
 import com.javaclasses.chatroom.entities.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/api/")
 public class RegistrationController {
 
+    @Autowired
+    private RegistrationService registrationService;
+
     @PostMapping("/signUp")
     @ResponseBody
     public String registrateUser(@RequestParam("login") String login, @RequestParam("password") String password) throws Exception {
         System.out.println("Login: " + login);
         System.out.println("Password: " + password);
-        validateRegistrationData(login, password);
-        return login;
-    }
-
-    public void validateRegistrationData(String login, String password) throws Exception {
-        if (login.isEmpty() || password.isEmpty() || password.contains(" ") || login.contains(" ") || password.length() < 8) {
-            throw new Exception();
+        try {
+            registrationService.registerUser(new User(login, password));
+        } catch (Exception ex) {
+            System.out.println("Catched: "  + ex);
         }
+        System.out.println("ready");
+        return login;
     }
 }
