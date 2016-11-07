0797a046b23ee228e733c7ce24f4e898d6f5e5ba
kalinindenys <kalinindenv@gmail.com>
2016-11-07 17:52:24 +0200

    -chatroom_tests: add Message entity with id, author, chatroom, content, and date fields

---
+++ persistence/src/main/java/com/javaclasses/chatroom/entities/Message.java
@@ -0,0 +1,51 @@
+package com.javaclasses.chatroom.entities;
+
+import java.util.Date;
+
+public class Message {
+    private Long id;
+    private User author;
+    private Chatroom chatroom;
+    private String content;
+    private Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Chatroom getChatroom() {
+        return chatroom;
+    }
+
+    public void setChatroom(Chatroom chatroom) {
+        this.chatroom = chatroom;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
