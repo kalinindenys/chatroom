e5da203ab8a07519fa453c2e30bc6922b9550fa1
kalinindenys <kalinindenv@gmail.com>
2016-11-10 12:49:55 +0200

    -chatroom_tests: make ChatroomRepository.java extend PagingAndSortingRepository

--- persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
+++ persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
@@ -2,8 +2,9 @@ package com.javaclasses.chatroom.persistence;
 
 import com.javaclasses.chatroom.persistence.entity.Chatroom;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatroomRepository extends CrudRepository<Chatroom, Long> {
+public interface ChatroomRepository extends PagingAndSortingRepository<Chatroom, Long> {
 }
