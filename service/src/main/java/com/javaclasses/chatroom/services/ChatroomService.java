16b6923634bdf1783eaa93f3d12ad3bcf120c7c7
kalinindenys <kalinindenv@gmail.com>
2016-11-10 12:52:47 +0200

    -chatroom_tests: refactor postMessage(Message message) to postMessage(MessageDTO message)

--- service/src/main/java/com/javaclasses/chatroom/services/ChatroomService.java
+++ service/src/main/java/com/javaclasses/chatroom/services/ChatroomService.java
@@ -3,11 +3,10 @@ package com.javaclasses.chatroom.services;
 import com.javaclasses.chatroom.persistence.entity.Chatroom;
 import com.javaclasses.chatroom.persistence.entity.Message;
 import com.javaclasses.chatroom.persistence.entity.User;
+import com.javaclasses.chatroom.services.DTO.MessageDTO;
 import com.javaclasses.chatroom.services.exception.EmptyMessageException;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Service
 public interface ChatroomService {
@@ -19,5 +18,5 @@ public interface ChatroomService {
 
     Iterable<User> getChatroomMemberList(Long chatroomId);
 
-    void postMessage(Message message) throws EmptyMessageException;
+    void postMessage(MessageDTO message) throws EmptyMessageException;
 }
