3a9fc4370f63fa86e16f1bc19275cfe126677286
kalinindenys <kalinindenv@gmail.com>
2016-11-11 18:51:40 +0200

    -chatroom_tests: delete unused imports

--- persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
+++ persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
@@ -1,7 +1,6 @@
 package com.javaclasses.chatroom.persistence;
 
 import com.javaclasses.chatroom.persistence.entity.Chatroom;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
