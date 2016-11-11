d6068f6ef5a7c455fb75f97df9cc1b1fbe161c7a
kalinindenys <kalinindenv@gmail.com>
2016-11-11 18:50:57 +0200

    -chatroom_tests: refactor imports

--- persistence/src/main/java/com/javaclasses/chatroom/persistence/entity/User.java
+++ persistence/src/main/java/com/javaclasses/chatroom/persistence/entity/User.java
@@ -1,9 +1,7 @@
 package com.javaclasses.chatroom.persistence.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User {
