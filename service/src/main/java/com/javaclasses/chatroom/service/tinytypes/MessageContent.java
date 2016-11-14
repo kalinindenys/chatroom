6d29e76d7af0f5c77037435a1c2b5e77d596ab86
kalinindenys <kalinindenv@gmail.com>
2016-11-14 12:06:28 +0200

    -chatroom_tests: add ChatroomId.java, MessageContent.java, MessageId.java tinytipes

---
+++ service/src/main/java/com/javaclasses/chatroom/service/tinytypes/MessageContent.java
@@ -0,0 +1,17 @@
+package com.javaclasses.chatroom.service.tinytypes;
+
+public class MessageContent {
+    String content;
+
+    public MessageContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
