d84b6488ce1605f763d0e6a1c6afee2186f1371d
kalinindenys <kalinindenv@gmail.com>
2016-11-14 17:45:57 +0200

    -chatroom_tests: add new method findAllByName() to ChatroomRepository.java.

--- persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
+++ persistence/src/main/java/com/javaclasses/chatroom/persistence/ChatroomRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatroomRepository extends PagingAndSortingRepository<Chatroom, Long> {
+    Iterable<Chatroom> findAllByName(String name);
 }
