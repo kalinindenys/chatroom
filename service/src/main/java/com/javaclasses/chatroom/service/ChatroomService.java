48185fb70466ab6f7ac35ff0977a90a37e61fc0e
kalinindenys <kalinindenv@gmail.com>
2016-11-14 17:46:26 +0200

    -chatroom_tests: add new method findChatroom(String name) to ChatroomService.java

--- service/src/main/java/com/javaclasses/chatroom/service/ChatroomService.java
+++ service/src/main/java/com/javaclasses/chatroom/service/ChatroomService.java
@@ -4,9 +4,6 @@ import com.javaclasses.chatroom.persistence.entity.Chatroom;
 import com.javaclasses.chatroom.persistence.entity.Message;
 import com.javaclasses.chatroom.persistence.entity.User;
 import com.javaclasses.chatroom.service.DTO.MessageDTO;
-import com.javaclasses.chatroom.service.EmptyMessageException;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
 
 public interface ChatroomService {
@@ -14,11 +11,13 @@ public interface ChatroomService {
 
     Chatroom getChatroom(Long chatroomId);
 
+    Iterable<Chatroom> findChatroom(String name);
+
     Iterable<Message> getMessages(Long chatroomId);
 
     Iterable<User> getChatroomMemberList(Long chatroomId);
 
     void postMessage(MessageDTO message, Long chatroomId) throws EmptyMessageException;
 
-    Page<Chatroom> getAllChatrooms(int pageCount);
+    Iterable<Chatroom> getAllChatrooms();
 }
