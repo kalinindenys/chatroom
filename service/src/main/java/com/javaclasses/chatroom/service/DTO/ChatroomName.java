15af08d3b09fde7fe8eb462a3d7ac2deffcab3dc
kalinindenys <kalinindenv@gmail.com>
2016-11-17 17:14:00 +0200

    -chatroom_tests: refactor ChatroomDTO.java to ChatroomName.java

---
+++ service/src/main/java/com/javaclasses/chatroom/service/DTO/ChatroomName.java
@@ -0,0 +1,17 @@
+package com.javaclasses.chatroom.service.DTO;
+
+public class ChatroomName {
+    private String name;
+
+    public ChatroomName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
