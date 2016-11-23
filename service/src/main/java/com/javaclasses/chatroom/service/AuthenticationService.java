9fe4747c8915c37b689380ba9850ffd0716788a2
dmytro.grankin <o-grank@yandex.ru>
2016-11-23 18:46:58 +0200

    -loginSubBranch: add javadoc for sign up

--- service/src/main/java/com/javaclasses/chatroom/service/AuthenticationService.java
+++ service/src/main/java/com/javaclasses/chatroom/service/AuthenticationService.java
@@ -7,6 +7,16 @@ import com.javaclasses.chatroom.service.dto.UserDTO;
 
 public interface AuthenticationService {
 
+    /**
+     * Constructs a new {@code User} with specified {@code login} and {@code password}.
+     * Then save it to user repository.
+     *
+     * @param login the login of user to be signed up.
+     * @param password the password of user to be signed up.
+     * @param passwordConfirmation the password confirmation.
+     * @throws LoginAlreadyExistsException if the {@code login} already exists in user repository.
+     * @throws PasswordConfirmationException if the {@code password} and {@code passwordConfirmation} do not match.
+     */
     void signUp(Login login, Password password, Password passwordConfirmation) throws LoginAlreadyExistsException, PasswordConfirmationException;
 
     SecurityTokenDTO signIn(Login login, Password password) throws AuthenticationException;
