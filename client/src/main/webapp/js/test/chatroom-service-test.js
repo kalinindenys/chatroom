19b1cafe0b7342e756778076647b647259c483af
dmytro.grankin <o-grank@yandex.ru>
2016-12-05 16:07:59 +0200

    -chatroomUI: add test runner

--- client/src/main/webapp/js/test/chatroom-service-test.js
+++ client/src/main/webapp/js/test/chatroom-service-test.js
@@ -1,7 +1,40 @@
-var ChatroomServiceTest = function () {
+// var ChatroomServiceTest = function () {
+//
+//     var test = function () {
+//
+//     };
+//
+// };
+//
 
-    var test = function () {
+var chatroomInMemoryStorage = new ChatroomInMemoryStorage();
+var chatroomService = new ChatroomService(chatroomInMemoryStorage);
 
-    };
+describe("findAll", function() {
 
-};
+    it("should return 0 elements", function() {
+
+        unitjs.value(chatroomService.findAll().length).isEqualTo(0);
+
+    });
+
+    it("should return one chatroom", function () {
+
+
+
+    });
+
+});
+
+describe("createChatroom", function () {
+
+    it("should create chatroom", function () {
+        var chatroomName = "chatroomName";
+        // var expectedChatroom = new ChatroomDTO(null, "chatroomName", );
+        var updatedChatrooms = chatroomService.createChatroom(chatroomName);
+
+        unitjs.string(actualChatroom.getName()).is(chatroomName);
+    });
+
+
+});
